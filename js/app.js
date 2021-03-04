6031a89cd433060b6823740c5add037580c39668
Erik Mortensen <mortensenerikm@gmail.com>
2021-03-03 22:05:29 -0300

    Lee datos busqueda

--- js/app.js
+++ js/app.js
@@ -1,11 +1,28 @@
 // Variables
-const resultado = document.querySelector('#resultado');
+const marca = document.querySelector('#marca');
 const year = document.querySelector('#year');
+const minimo = document.querySelector('#minimo');
+const maximo = document.querySelector('#maximo');
+const puertas = document.querySelector('#puertas');
+const transmision = document.querySelector('#transmision');
+const color = document.querySelector('#color');
+
+
+const resultado = document.querySelector('#resultado');
 
 const max = new Date().getFullYear();
 const min = max - 10;
 
-
+// Generar un objeto con la busqueda
+const datosBusquda = {
+    marca: '',
+    year: '',
+    minimo: '',
+    maximo: '',
+    puertas: '',
+    transmision: '',
+    color: '',
+}
 
 // Eventos
 document.addEventListener('DOMContentLoaded', () => {
@@ -15,6 +32,37 @@ document.addEventListener('DOMContentLoaded', () => {
 
 });
 
+
+// Event listener para los select de busqueda
+marca.addEventListener('change', e => {
+    datosBusquda.marca = e.target.value;
+});
+
+year.addEventListener('change', e => {
+    datosBusquda.year = e.target.value;
+});
+
+minimo.addEventListener('change', e => {
+    datosBusquda.minimo = e.target.value;
+});
+
+maximo.addEventListener('change', e => {
+    datosBusquda.maximo = e.target.value;
+});
+
+puertas.addEventListener('change', e => {
+    datosBusquda.puertas = e.target.value;
+});
+
+transmision.addEventListener('change', e => {
+    datosBusquda.transmision = e.target.value;
+});
+
+color.addEventListener('change', e => {
+    datosBusquda.color = e.target.value;
+
+    console.log(datosBusquda);
+});
 
 
 // Funciones
